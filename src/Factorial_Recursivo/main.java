1c23144503539c805da65501d291592c4d7a2e20
Stiven_2000 <esromero@tecnologicoloja.edu.ec>
2020-08-21 15:44:30 -0500

    Recursividad_Fibonacci

---
+++ src/Factorial_Recursivo/main.java
@@ -0,0 +1,14 @@
+
+package Factorial_Recursivo;
+
+import java.util.Scanner;
+
+public class main {
+    
+    public static void main(String[] args) {
+        Factorial llamar = new Factorial();
+        System.out.println("\nLa respuesta es: "+llamar.fibonacciRecursivo(12));
+
+      
+    }
+}
